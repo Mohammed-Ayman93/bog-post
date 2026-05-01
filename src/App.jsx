01f39ce722c377a3ec06299a669e0aff89a28f4e
Mohammed Ayman <m7md.ayman1993@gmail.com>
2026-05-02 00:35:20 +0300

    redeploy frontend

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ export const AuthContext = createContext();
 // ------------------ api ----------------------- //
 
   export const api = axios.create({
-    baseURL: "import.meta.env.VITE_API_URL",
+    baseURL: import.meta.env.VITE_API_URL,
     withCredentials: true,
   });
 api.interceptors.response.use(
